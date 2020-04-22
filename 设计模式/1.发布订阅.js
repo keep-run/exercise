f4af49a7ec023ccc5df6d21c6e0f71c898e44941
王治春 <wangzhichun001@ke.com>
2020-04-22 19:10:01 +0800

    增加发布订阅模式

---
+++ 设计模式/1.发布订阅.js
@@ -0,0 +1,39 @@
+// 发布订阅模式最简易的实现。
+
+const publish = {}  //发布者
+publish.clientList = []   //订阅的事件列表
+
+// 将订阅事件加入事件列表
+publish.listen = function (fn) {
+  this.clientList.push(fn)
+}
+
+//移除订阅事件
+publish.remove = function (_fn) {
+  // 不传，取消所有
+  let clientList
+  if (!_fn) {
+    this.clientList = []
+  } else {
+    this.clientList= this.clientList.filter(fn =>{return fn !== _fn})
+  }
+}
+
+// 触发事件
+publish.trigger = function (...args) {
+  this.clientList.forEach(event => event.apply(publish, args))
+}
+
+
+
+const consl = function (args) {
+  console.log('args:', args)
+}
+const consl1 = function (args) {
+  console.log('args-again:', args)
+}
+publish.listen(consl)
+publish.listen(consl1)
+publish.remove(consl)
+
+publish.trigger('111')
